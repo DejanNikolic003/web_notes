05e48e59d3b12bcd420d03c5c7b00c3dda4dcfc8
DejanNikolic003 <knolekjhnny@gmail.com>
2025-09-04 11:54:28 +0200

    refactor(auth): improve auth initialization

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,14 +5,25 @@ import Register from "./pages/auth/Register";
 import Login from "./pages/auth/Login";
 import { useEffect } from "react";
 import { me } from "./actions/auth";
+import Notes from "./pages/notes/Notes";
+import Home from "./pages/Home";
+import { useState } from "react";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   const dispatch = useDispatch();
   const user = useSelector((state) => state.auth);
+  const [initialized, setInitialized] = useState(false);
 
   useEffect(() => {
-    dispatch(me());
-  });
+    const initAuth = async () => {
+      await dispatch(me());
+      setInitialized(true);
+    };
+    initAuth();
+  }, [dispatch]);
+
+  if (!initialized) return <h2>Loading...</h2>; // wait for auth
 
   return (
     <>
@@ -27,12 +38,20 @@ function App() {
           ) : (
             <div>
               <h1>You are logged in as {user?.data?.username}</h1>
-              <Link to="/login">Login</Link>
+              <Link to="/">Home</Link>
             </div>
           )}
         </nav>
 
         <Routes>
+          <Route
+            path="/"
+            element={
+              <ProtectedRoute>
+                <Home />{" "}
+              </ProtectedRoute>
+            }
+          />
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
         </Routes>
