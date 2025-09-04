26c9010c9d985d4a08d256d23a2c2a659cb04d0c
DejanNikolic003 <knolekjhnny@gmail.com>
2025-09-04 11:55:36 +0200

    feath(notes): note state

--- frontend/src/reducers/notes.js
+++ frontend/src/reducers/notes.js
@@ -1,5 +1,21 @@
-const notesReducer = (state = {}, action) => {
+import {
+  FETCH_ALL,
+  FETCH_BY_SEARCH,
+  CREATE,
+  UPDATE,
+  DELETE,
+  START_LOADING,
+  STOP_LOADING,
+} from "../constants/actionTypes";
+
+const notesReducer = (state = { isLoading: true, notes: [] }, action) => {
   switch (action.type) {
+    case FETCH_ALL:
+      // console.log(action.payload);
+      return {
+        ...state,
+        notes: action.payload,
+      };
     default:
       return state;
   }
