e6bdbba4fe7ed8113a76c5480f4d26cfdc315405
DejanNikolic003 <knolekjhnny@gmail.com>
2025-09-04 11:53:14 +0200

    feat(notes): add new action types for notes, and loading.

--- frontend/src/constants/actionTypes.js
+++ frontend/src/constants/actionTypes.js
@@ -1,3 +1,15 @@
+// AUTH
 export const AUTH = "AUTH";
 export const LOGOUT = "LOGOUT";
 export const AUTH_ERROR = "AUTH_ERROR";
+
+// NOTES
+export const FETCH_ALL = "FETCH_ALL";
+export const FETCH_BY_SEARCH = "FETCH_BY_SEARCH";
+export const CREATE = "CREATE";
+export const UPDATE = "UPDATE";
+export const DELETE = "DELETE";
+
+// LOADING
+export const START_LOADING = "START_LOADING";
+export const STOP_LOADING = "STOP_LOADING";
