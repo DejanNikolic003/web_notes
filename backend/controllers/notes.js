b94b2c5f4fab988671cb176e6824380cbe2bc10a
DejanNikolic003 <knolekjhnny@gmail.com>
2025-09-09 09:49:22 +0200

    refactor(notes): removed shared column, added sorting by pinned notes

--- backend/controllers/notes.js
+++ backend/controllers/notes.js
@@ -3,7 +3,8 @@ import Note from "../models/notes.js";
 export const listNotes = async (req, res) => {
   try {
     const { user } = req;
-    const notes = await Note.find({ user: user._id });
+    // TODO: Add order by pinned
+    const notes = await Note.find({ user: user._id }).sort({ pinned: -1 });
 
     res.status(200).json({ message: "Successfully retrieved!", notes });
   } catch (error) {
@@ -19,12 +20,6 @@ export const getNoteById = async (req, res) => {
 
     if (!note) return res.status(404).json({ message: "Note not found!" });
 
-    if (!note.shared && !note.user.equals(user._id)) {
-      return res
-        .status(403)
-        .json({ message: "You don't have access to view this note!" });
-    }
-
     res.status(200).json({ message: "Successfully retrieved!", note });
   } catch (error) {
     res.status(500).json({ message: error.message });
@@ -35,15 +30,14 @@ export const createNote = async (req, res) => {
   try {
     const { title, content, pinned, shared } = req.body;
     const { user } = req;
+    console.log(req.body);
 
     const note = await Note.create({
       user: user._id,
       title: title,
       content: content,
       pinned: pinned,
-      shared: shared,
     });
-
     res.status(200).json({ message: "Successfully created!", note });
   } catch (error) {
     res.status(500).json({ message: error.message });
@@ -67,7 +61,6 @@ export const editNote = async (req, res) => {
     note.title = title;
     note.content = content;
     note.pinned = pinned;
-    note.shared = shared;
     await note.save();
 
     res.status(200).json({ message: "Successfully edited!", note });
